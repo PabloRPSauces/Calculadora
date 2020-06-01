7458117e62c8fb308b951c78a7977a9fdb311d61
Pablo <Pablo@DESKTOP-HF5M7M3>
2020-06-01 08:52:31 +0200

    Primer Commit

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Version 1.0 
  */
 
 /**
@@ -17,7 +15,7 @@ public class Calculadora {
     public int suma(int num1,int num2){
         return num1+num2;
     }
-    
+
     public int resta(int num1,int num2){
         return num1-num2;
     }
